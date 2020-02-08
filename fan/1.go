cbc5debcde2b5e8e17a42c46ae415e90181414df
pranav <ambhore.pranav@gmail.com>
2020-02-08 16:14:18 +0530

    fan in-out challenge done for factorial

---
+++ fan/1.go
@@ -0,0 +1,74 @@
+package main
+
+import "fmt"
+
+func main() {
+	var numOfWorkers int
+	fmt.Scanf("%d", &numOfWorkers)
+
+	c := make(chan int)
+	var o []chan map[int]int
+	done := make(chan bool)
+	collate := make(chan map[int]int)
+
+	go func() {
+		for i := 0; i < 100; i++ {
+			for j := 0; j < 20; j++ {
+				c <- j
+			}
+		}
+		close(c)
+	}()
+
+	for s := 0; s < numOfWorkers; s++ {
+		out := fact(c, done, s)
+		o = append(o, out)
+	}
+
+	consume(done, collate, o...)
+
+	go func() {
+		for s := 0; s < numOfWorkers; s++ {
+			<-done
+		}
+		close(collate)
+	}()
+
+	for i := range collate {
+		fmt.Println(i)
+	}
+
+}
+
+func fact(cin <-chan int, done chan bool, routineName int) chan map[int]int {
+	cout := make(chan map[int]int)
+	go func() {
+		counter := 0
+		for i := range cin {
+			out := 1
+			for j := i; j > 1; j-- {
+				out *= j
+			}
+			cout <- map[int]int{i: out}
+			counter++
+		}
+		fmt.Println("Goroutine fact ", routineName, "processed", counter, "items")
+		close(cout)
+		fmt.Println("Goroutine fact", routineName, "is finished")
+	}()
+	return cout
+}
+
+func consume(done chan bool, collate chan map[int]int, c ...chan map[int]int) {
+	for idx, ci := range c {
+		go func(ci chan map[int]int, idx int) {
+			counter := 0
+			for i := range ci {
+				collate <- i
+				counter++
+			}
+			fmt.Println("Goroutine consume ", idx, "consumed", counter, "items")
+			done <- true
+		}(ci, idx)
+	}
+}
